7427983d5cd4c5e5d9304bf123315965c3b8cff9
Denise Magner <deerenae@gmail.com>
2020-02-17 02:21:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,28 @@
 def nyc_pigeon_organizer(data)
   hash = {}
-  data.each do |attributes, |
+  data.each do |attributes, value|
+    value.each do |feature, array|
+      array.each do |array|
+        if hash[name] == nil 
+          hash[name] = {}
+           hash[name][attributes] = []
+        else 
+          hash[name][attributes] = []
+        end   
+      end 
+    end
+  hash.each do |name, value|
+    value.each do |hashvalue, array|
+      data.each do |attributes, values|
+        values.each do |value, array|
+          array.each do |element|
+            if element == name && hashvalue == attributes
+              hash[name][hashvalue] << value.to_s 
+            end 
+          end 
+        end
+      end 
+    end 
+  end
+  hash
 end
